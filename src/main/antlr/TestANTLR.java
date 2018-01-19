1765eb6e7808b795be85d1852ac21f43c4c0a228
satnam-sandhu <singhsandhusatnam@outlook.com>
2018-01-19 13:21:15 +0530

    getting staerted with ANTLR

--- src/main/antlr/TestANTLR.java
+++
@@ -1,7 +0,0 @@
-package antlr;
-
-public class TestANTLR {
-    public void saySomething(String something) {
-        System.out.println("Hello "+something);
-    }
-}
--- src/main/com/kitcode/ASTGenerator.java
+++ src/main/com/kitcode/ASTGenerator.java
@@ -5,8 +5,6 @@ import org.antlr.v4.runtime.*;
 
 public class ASTGenerator {
     public static void main(String args[]) {
-	TestANTLR obj = new TestANTLR();
-	obj.saySomething("Satnam");
         System.out.println("Hello World");
     }
 }
