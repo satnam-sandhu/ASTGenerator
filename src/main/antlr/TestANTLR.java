aef354da18462767210afb7f72f4b01fe1e838a3
satnam-sandhu <singhsandhusatnam@outlook.com>
2018-01-19 12:58:19 +0530

    need to learn the compilcated java build process and will focus on antlr

---
+++ src/main/antlr/TestANTLR.java
@@ -0,0 +1,7 @@
+package antlr;
+
+public class TestANTLR {
+    public void saySomething(String something) {
+        System.out.println("Hello "+something);
+    }
+}
--- src/main/com/kitcode/ASTGenerator.java
+++ src/main/com/kitcode/ASTGenerator.java
@@ -1,9 +1,12 @@
 package com.kitcode;
 
+import antlr.*;
 import org.antlr.v4.runtime.*;
 
 public class ASTGenerator {
     public static void main(String args[]) {
+	TestANTLR obj = new TestANTLR();
+	obj.saySomething("Satnam");
         System.out.println("Hello World");
     }
 }
