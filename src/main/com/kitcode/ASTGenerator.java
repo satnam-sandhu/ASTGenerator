65f5fe4904d1444c88eb6e9326a01ab2deb4025d
satnam-sandhu <singhsandhusatnam@outlook.com>
2018-01-19 13:51:06 +0530

    almost done with parsing the source code

--- src/main/com/kitcode/ASTGenerator.java
+++ src/main/com/kitcode/ASTGenerator.java
@@ -5,6 +5,15 @@ import org.antlr.v4.runtime.*;
 
 public class ASTGenerator {
     public static void main(String args[]) {
-        System.out.println("Hello World");
+        String input = "public class HelloWord {" +
+            "public void print(String r){" +
+            "for(int i = 0;true;i+=2)" +
+            "System.out.println(r);" +
+            "}" +
+            "}";
+	Java8Lexer lexer = new Java8Lexer(new ANTLRInputStream(input));
+	CommonTokenStream tokens = new CommonTokenStream(lexer);
+	Java8Parser parser = new Java8Parser(tokens);
+
     }
 }
