2a3cf585621482cc3fa7e3e8c2f580f21adc49e4
satnam-sandhu <singhsandhusatnam@outlook.com>
2018-01-21 14:00:43 +0530

    beautified

--- src/main/com/kitcode/ASTGenerator.java
+++ src/main/com/kitcode/ASTGenerator.java
@@ -13,27 +13,28 @@ import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.RuleContext;
 import org.antlr.v4.runtime.tree.ParseTree;
 
-public class ASTGenerator{
-    
-    public static String readFile() throws IOException{
+public class ASTGenerator {
+
+    public static String readFile() throws IOException {
         File file = new File("resource/java/Blabla.java");
         byte[] encoded = Files.readAllBytes(file.toPath());
         return new String(encoded, Charset.forName("UTF-8"));
     }
-    
+
     public static void main(String args[]) throws IOException {
-        String input = readFile();
-	Java8Lexer lexer = new Java8Lexer(new ANTLRInputStream(input));
-	CommonTokenStream tokens = new CommonTokenStream(lexer);
-	Java8Parser parser = new Java8Parser(tokens);
+        String inputString = readFile();
+        ANTLRInputStream input = new ANTLRInputStream(inputString);
+        Java8Lexer lexer = new Java8Lexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        Java8Parser parser = new Java8Parser(tokens);
         ParserRuleContext ctx = parser.classDeclaration();
-        
+
         printAST(ctx, false, 0);
     }
-    
+
     private static void printAST(RuleContext ctx, boolean verbose, int indentation) {
-        boolean toBeIgnored = !verbose && ctx.getChildCount() == 1
-                && ctx.getChild(0) instanceof ParserRuleContext;
+        boolean toBeIgnored = !verbose && ctx.getChildCount() == 1 && ctx.getChild(0) instanceof ParserRuleContext;
+
         if (!toBeIgnored) {
             String ruleName = Java8Parser.ruleNames[ctx.getRuleIndex()];
             for (int i = 0; i < indentation; i++) {
@@ -41,10 +42,10 @@ public class ASTGenerator{
             }
             System.out.println(ruleName + " -> " + ctx.getText());
         }
-        for (int i=0;i<ctx.getChildCount();i++) {
+        for (int i = 0; i < ctx.getChildCount(); i++) {
             ParseTree element = ctx.getChild(i);
             if (element instanceof RuleContext) {
-                printAST((RuleContext)element, verbose, indentation + (toBeIgnored ? 0 : 1));
+                printAST((RuleContext) element, verbose, indentation + (toBeIgnored ? 0 : 1));
             }
         }
     }
