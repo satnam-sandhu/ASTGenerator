728010e6fef02bbba0f4b8705e7e5d7e83467d8f
satnam-sandhu <singhsandhusatnam@outlook.com>
2018-01-25 17:10:58 +0530

    added arrayList

--- resource/java/Blabla.java
+++ resource/java/Blabla.java
@@ -1,10 +1,9 @@
 class example{
-	public static void print(int a){
-		int b = a +1;
-		System.out.println(b);
-	}
-
 	public static void main(){
-		print(15);
+		int a;
+		int b;
+		a = 1;
+		b = 5;
+		int c = a + b;
 	}
 }
--- src/main/com/kitcode/ASTGenerator.java
+++ src/main/com/kitcode/ASTGenerator.java
@@ -4,6 +4,8 @@ import antlr.*;
 import java.io.File;
 import java.io.IOException;
 
+import java.util.ArrayList;
+
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 
@@ -15,24 +17,32 @@ import org.antlr.v4.runtime.tree.ParseTree;
 
 public class ASTGenerator {
 
+    static ArrayList<String> LineNum = new ArrayList<String>();
+    static ArrayList<String> Type = new ArrayList<String>();
+    static ArrayList<String> Content = new ArrayList<String>();
+
     public static String readFile() throws IOException {
         File file = new File("resource/java/Blabla.java");
         byte[] encoded = Files.readAllBytes(file.toPath());
         return new String(encoded, Charset.forName("UTF-8"));
     }
 
-    public static void main(String args[]) throws IOException {
+    public static void main(String args[]) throws IOException{
         String inputString = readFile();
         ANTLRInputStream input = new ANTLRInputStream(inputString);
         Java8Lexer lexer = new Java8Lexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         Java8Parser parser = new Java8Parser(tokens);
-        ParserRuleContext ctx = parser.classDeclaration();
+        ParserRuleContext ctx = parser.compilationUnit();
 
-        printAST(ctx, false, 0);
+        generateAST(ctx, false, 0);
+	System.out.println(LineNum);
+        System.out.println(Type);
+        System.out.println(Content);
+
     }
 
-    private static void printAST(RuleContext ctx, boolean verbose, int indentation) {
+    private static void generateAST(RuleContext ctx, boolean verbose, int indentation) {
         boolean toBeIgnored = !verbose && ctx.getChildCount() == 1 && ctx.getChild(0) instanceof ParserRuleContext;
 
         if (!toBeIgnored) {
@@ -41,11 +51,14 @@ public class ASTGenerator {
                 System.out.print("  ");
             }
             System.out.println(ruleName + " -> " + ctx.getText());
-        }
+	    LineNum.add(Integer.toString(indentation));
+            Type.add(ruleName);
+            Content.add(ctx.getText());
+	}
         for (int i = 0; i < ctx.getChildCount(); i++) {
             ParseTree element = ctx.getChild(i);
             if (element instanceof RuleContext) {
-                printAST((RuleContext) element, verbose, indentation + (toBeIgnored ? 0 : 1));
+                generateAST((RuleContext) element, verbose, indentation + (toBeIgnored ? 0 : 1));
             }
         }
     }
